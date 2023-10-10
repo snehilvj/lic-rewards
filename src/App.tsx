cb32966f49da4905ec4a8c97efa1c4550e0a52c3
Snehil Vijay <snehilvj@outlook.com>
2023-10-10 11:18:40 +0530

    :sparkles: Add formula based rewards

--- src/App.tsx
+++ src/App.tsx
@@ -7,281 +7,109 @@ import {
   Card,
   Center,
   Stack,
+  Paper,
 } from "@mantine/core";
 import { theme } from "./theme";
 import { useEffect, useState } from "react";
-import { Children } from "react";
+import {
+  GrandParty,
+  LuckyDraw,
+  ToppersTop,
+  TyohaarKUphaar,
+} from "./competetions";
+
+const texts = {
+  title:
+    "प्रीमियम आधार पर ( सभी प्लान पर ) सिंगल और नॉन सिंगल पर प्राप्त होने वाले इनाम  दिनांक 1-10-23 से 31 -10-2023  तक  ( मिनिमम 1 लाख प्रीमियम )",
+  tyohaar:
+    "त्योहार के उपहार (SP+NSP ) ZO 01-10-23 से 31 -10-23 तक (Every ananda extra 100 रु)",
+  toppers: "टोप टॉपर्स  CO (SP+NSP)01-10-23 से 30-11-23 तक",
+  total: "कुल इनाम",
+  party:
+    "होटल रामी रॉयल में आकर्षक एंजोयमेन्ट प्रोग्राम (One night stay) दिनांक- 03-10-23 से 27-10-23 तक",
+  luckyDraw: "Lucky Draw Coupons",
+};
 
 interface RewardProps {
   text: string;
+  value?: string;
 }
 
 const Reward = (props: RewardProps) => {
   return (
     <Card withBorder>
-      <Center h={100}>
+      <Stack mih={80} justify="center">
         <Text size="lg">{props.text}</Text>
-      </Center>{" "}
+        <Text color="indigo" size="lg">
+          {props.value}
+        </Text>
+      </Stack>
     </Card>
   );
 };
 
-function calculateRewards(amount: any) {
-  const table = [
-    { amount: 100000, cash: 300, bonus: 0, special1: "", special2: "" },
-    { amount: 200000, cash: 750, bonus: 0, special1: "", special2: "" },
-    { amount: 300000, cash: 750, bonus: 0, special1: "", special2: "" },
-    { amount: 400000, cash: 750, bonus: 0, special1: "", special2: "" },
-    { amount: 500000, cash: 2500, bonus: 0, special1: "", special2: "" },
-    { amount: 600000, cash: 2500, bonus: 0, special1: "", special2: "" },
-    { amount: 700000, cash: 2500, bonus: 0, special1: "", special2: "" },
-    { amount: 800000, cash: 2500, bonus: 0, special1: "", special2: "" },
-    { amount: 900000, cash: 2500, bonus: 0, special1: "", special2: "" },
-    {
-      amount: 1000000,
-      cash: 10000,
-      bonus: 0,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "",
-    },
-    {
-      amount: 1100000,
-      cash: 11000,
-      bonus: 6000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1200000,
-      cash: 12000,
-      bonus: 6000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1300000,
-      cash: 13000,
-      bonus: 6000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1400000,
-      cash: 14000,
-      bonus: 6000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1500000,
-      cash: 15000,
-      bonus: 6000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1600000,
-      cash: 16000,
-      bonus: 9000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1700000,
-      cash: 17000,
-      bonus: 9000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1800000,
-      cash: 18000,
-      bonus: 9000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 1900000,
-      cash: 19000,
-      bonus: 9000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 2000000,
-      cash: 20000,
-      bonus: 9000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - night stay - Ramec Royal",
-    },
-    {
-      amount: 2100000,
-      cash: 21000,
-      bonus: 12000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2200000,
-      cash: 22000,
-      bonus: 12000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2300000,
-      cash: 23000,
-      bonus: 12000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2400000,
-      cash: 24000,
-      bonus: 12000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2500000,
-      cash: 25000,
-      bonus: 12000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2600000,
-      cash: 26000,
-      bonus: 15000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2700000,
-      cash: 27000,
-      bonus: 15000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2800000,
-      cash: 28000,
-      bonus: 15000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 2900000,
-      cash: 29000,
-      bonus: 15000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 3000000,
-      cash: 30000,
-      bonus: 15000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "Seminar - with spouse",
-    },
-    {
-      amount: 3100000,
-      cash: 31000,
-      bonus: 18000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "3 tickets Ramec Royal",
-    },
-    {
-      amount: 3200000,
-      cash: 32000,
-      bonus: 18000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "3 tickets Ramec Royal",
-    },
-    {
-      amount: 3300000,
-      cash: 33000,
-      bonus: 18000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "3 tickets Ramec Royal",
-    },
-    {
-      amount: 3400000,
-      cash: 34000,
-      bonus: 18000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "3 tickets Ramec Royal",
-    },
-    {
-      amount: 3500000,
-      cash: 35000,
-      bonus: 18000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "3 tickets Ramec Royal",
-    },
-    {
-      amount: 3600000,
-      cash: 36000,
-      bonus: 21000,
-      special1: "Lucky Draw (2 lacs)",
-      special2: "4 tickets Ramec Royal",
-    },
-  ];
-
-  let rewards = [];
-
-  for (let i = table.length - 1; i >= 0; i--) {
-    if (amount >= table[i].amount) {
-      const row = table[i];
-      rewards.push("₹" + row.cash.toString());
-
-      if (row.bonus > 0) {
-        rewards.push("₹" + row.bonus.toString());
-      }
-
-      if (row.special1) {
-        rewards.push(row.special1);
-      }
-
-      if (row.special2) {
-        rewards.push(row.special2);
-      }
-
-      break;
-    }
-  }
-  return rewards;
-}
-
 export default function App() {
-  const [val, setVal] = useState<any>(1);
-  const [rewards, setRewards] = useState<string[]>([]);
+  const [val, setVal] = useState<any>(0);
+  const [topperReward, setTopperReward] = useState(ToppersTop(val));
+  const [tyohaarReward, setTyohaarReward] = useState(TyohaarKUphaar(val));
+  const [luckyDraw, setLuckyDraw] = useState(LuckyDraw(val));
+  const [partyReward, setPartyReward] = useState(GrandParty(val));
 
   useEffect(() => {
-    const rew = calculateRewards(val * 100000);
-    setRewards(rew);
+    setTopperReward(ToppersTop(val));
+  }, [val]);
+  useEffect(() => {
+    setTyohaarReward(TyohaarKUphaar(val));
+  }, [val]);
+  useEffect(() => {
+    setLuckyDraw(LuckyDraw(val));
+  }, [val]);
+  useEffect(() => {
+    setPartyReward(GrandParty(val));
   }, [val]);
 
   return (
     <MantineProvider theme={theme}>
       <Container p={50}>
-        <Text size="30" lh={0}>
-          LIC Agent Rewards
+        <Text size="30" lh={1.6}>
+          {texts.title}
         </Text>
         <NumberInput
           min={0}
-          max={40}
           size="lg"
           label="Amount"
-          description="Enter the amount in lacs"
           value={val}
+          hideControls
           onChange={setVal}
           py={50}
         />
         <Stack>
-          {Children.map(rewards, (text, index) => {
-            return <Reward key={index} text={text} />;
-          })}
+          {tyohaarReward ? (
+            <Paper withBorder p={10}>
+              {tyohaarReward ? (
+                <Reward text={texts.tyohaar} value={tyohaarReward.toString()} />
+              ) : null}
+              {topperReward ? (
+                <Reward text={texts.toppers} value={topperReward.toString()} />
+              ) : null}
+              <Reward
+                text={texts.total}
+                value={(topperReward + tyohaarReward).toString()}
+              />
+            </Paper>
+          ) : null}
+          {luckyDraw ? (
+            <Stack>
+              <Center>+</Center>
+              <Reward text={texts.luckyDraw} value="Upto 2 lacs" />
+            </Stack>
+          ) : null}
+          {partyReward ? (
+            <Stack>
+              <Center>+</Center>
+              <Reward text={texts.party} value={partyReward} />
+            </Stack>
+          ) : null}
         </Stack>
       </Container>
     </MantineProvider>
---
+++ src/competetions.tsx
@@ -0,0 +1,45 @@
+export const TyohaarKUphaar = (amount: number) => {
+  let res;
+  if (amount < 1e5) {
+    res = 0;
+  } else if (amount < 2 * 1e5) {
+    res = 300;
+  } else if (amount < 5 * 1e5) {
+    res = 750;
+  } else if (amount < 11 * 1e5) {
+    res = 2500;
+  } else {
+    const blocks = Math.floor((amount - 11e5) / 5e5);
+    res = 6000 + 3000 * blocks;
+  }
+  return Math.min(res, 2e5);
+};
+
+export const ToppersTop = (amount: number) => {
+  let res;
+  if (amount >= 10e5) {
+    res = Math.round(amount / 100);
+  } else {
+    res = 0;
+  }
+  return Math.min(res, 5e7);
+};
+
+export const LuckyDraw = (amount: number) => {
+  const blocks = Math.floor(amount / 10e5);
+  return blocks;
+};
+
+export const GrandParty = (amount: number) => {
+  if (amount >= 36e5) {
+    return "With spouse + 2 extra participants";
+  } else if (amount >= 31e5) {
+    return "With spouse + 1 extra participant";
+  } else if (amount >= 21e5) {
+    return "With spouse";
+  } else if (amount >= 11e5) {
+    return "Single";
+  } else {
+    return null;
+  }
+};
